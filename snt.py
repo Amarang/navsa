375401178e9985739c2af1d17918a5851d7e18f4
Nick Amin <namin@squark.physics.ucsb.edu>
2015-09-08 09:33:20 -0700

    new snt vidyo link

--- snt.py
+++ snt.py
@@ -12,7 +12,7 @@ import datetime
 def getSNT():
     output, outputDetail = "", ""
     if( datetime.datetime.now().isoweekday() in [2, 5] ):
-        output += "There's an SNT meeting at 9:30am today. Vidyo link: https://goo.gl/k3X6Is"
+        output += "There's an SNT meeting at 9:30am today. Vidyo link: https://vidyoportal.cern.ch/flex.html?roomdirect.html&key=eQuolbviCq96Fdc7pGgbcPdIY"
 
     return output, outputDetail
 
