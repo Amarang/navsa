fc4805bd9b75dff3d080740e5cc6447c5926d8eb
Nick <typhoid.pwns@gmail.com>
2016-01-22 18:21:54 -0800

    more

---
+++ speech/Process.py
@@ -0,0 +1,112 @@
+import wave, os, warnings
+from tqdm import tqdm
+import matplotlib as mpl
+mpl.use('Agg')
+import numpy as np
+import matplotlib.pyplot as plt
+from Split import Splitter
+from Fingerprint import Fingerprinter
+import Utils as u
+import scipy.ndimage as ndimage
+from sklearn import linear_model, metrics
+from sklearn.cross_validation import train_test_split
+
+
+
+class Processor:
+    def __init__(self, Ntime=30, Nfreq=30):
+        self.Ntime,self.Nfreq = 30,30
+        self.YXtot = []
+
+
+
+    def getFeatures(self, data, framerate, isSignal):
+        fp = Fingerprinter()
+        fp.setData(data, framerate)
+        times,freqs,Pxx = fp.getSpectrogram()
+
+        ztime = float(self.Ntime) / Pxx.shape[1] if Pxx.shape[1] > self.Ntime else 1
+        zfreq = float(self.Nfreq) / Pxx.shape[0] if Pxx.shape[0] > self.Nfreq else 1
+
+        # print zfreq,ztime
+        Pxx = ndimage.interpolation.zoom(Pxx,zoom=(zfreq, ztime),order=0)
+
+
+        nfreq,ntime = Pxx.shape
+        freqPad = self.Nfreq-nfreq # pad higher frequencies with 0
+        timePad = self.Ntime-ntime # pad higher times with 0
+        Pxx = np.pad(Pxx, [(freqPad,0), (0,timePad)], mode='constant', constant_values=0.0)
+
+        img = Pxx.flatten() # restore freq,time matrix with Pxx.reshape(self.Nfreq,self.Ntime)
+        img = np.insert( img, 0, [isSignal] ) # put 0 or 1 at beginning for truth value
+
+        return img
+
+    def processTrainingSet(self, basedir="sounds/train/", signalword="oknavsa", savedir="data/"):
+
+        clips = os.listdir(basedir)
+
+        sp = Splitter()
+
+        self.YXtot = []
+
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore",category=Warning)
+
+            for clip in clips:
+
+                sp.doSplit(basedir+clip)
+                subsamples = sp.getSubsamples()
+                framerate = sp.getFramerate()
+
+                isSignal = int(signalword in clip.lower())
+
+                for ss in subsamples:
+                    img = self.getFeatures(ss, framerate, isSignal)
+                    self.YXtot.append(img)
+
+        self.YXtot = np.array(self.YXtot)
+
+        outputname = "%simagedata_%i_%i.npy" % (savedir,self.Nfreq,self.Ntime)
+        np.save(outputname, self.YXtot)
+        print "made %s" % outputname
+        return outputname
+
+
+    def nudge(self, Xtot, Ytot, w=5):
+        newXtot = []
+        newYtot = np.array([Ytot for _ in range(3)]).ravel(1)
+        for x in Xtot:
+            xmat = x.reshape(self.Nfreq,self.Ntime)
+            left = xmat[:,range(w,self.Ntime)]
+            left = np.pad(left, [(0,0), (w,0)], mode='constant', constant_values=0.0)
+            right = xmat[:,range(0,self.Ntime-w)]
+            right = np.pad(right, [(0,0), (0,w)], mode='constant', constant_values=0.0)
+            newXtot.append(xmat.flatten())
+            newXtot.append(left.flatten())
+            newXtot.append(right.flatten())
+        return np.array(newXtot), newYtot
+
+    def loadTrainData(self, datafile):
+        self.Nfreq, self.Ntime = map(int,datafile.split(".")[0].split("_")[-2:])
+        self.YXtot = np.load(datafile)
+
+
+    def trainAndTest(self):
+        Xtot = self.YXtot[:,range(1,len(self.YXtot[0]))]
+        Ytot = self.YXtot[:,0]
+
+        Xtot, Ytot = self.nudge(Xtot,Ytot)
+
+        X_train, X_test, Y_train, Y_test = train_test_split(Xtot, Ytot, test_size=0.75, random_state=42)
+
+        logistic_classifier = linear_model.LogisticRegression(C=100.0)
+        logistic_classifier.fit(X_train, Y_train)
+
+        # print "Logistic regression using raw pixel features:\n%s\n" % (metrics.classification_report(Y_test,logistic_classifier.predict(X_test)))
+
+        print logistic_classifier.score(X_train, Y_train)
+        print logistic_classifier.score(X_test, Y_test)
+
+    def predict(self, data):
+        pass
