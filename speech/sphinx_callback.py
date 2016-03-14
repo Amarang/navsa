c6305a1622e60401880ac886a80078268040b319
Nick Amin <namin@squark.physics.ucsb.edu>
2016-03-14 15:58:13 -0700

    draw bar

--- speech/sphinx_callback.py
+++ speech/sphinx_callback.py
@@ -3,6 +3,7 @@ from pocketsphinx.pocketsphinx import *
 from sphinxbase.sphinxbase import *
 from collections import deque
 import pyaudio
+import audioop
 
 import Utils as u
 
@@ -10,6 +11,13 @@ class Listener:
     def __init__(self, hmm_type=1, vad_threshold=3.5, pl_window=10, wip=1e-4, silprob=0.3, bestpath=True):
         self.CHUNK = 1024
         self.RATE = 16000
+        self.FORMAT = pyaudio.paInt16
+
+        self.RUN_SECONDS = 100000
+
+        self.sampwidth = pyaudio.get_sample_size(self.FORMAT)
+        print self.sampwidth
+
         # Create a decoder with certain model
         self.config = Decoder.default_config()
 
@@ -37,7 +45,8 @@ class Listener:
         self.decoder = Decoder(self.config)
         self.t1_d = time.time()
 
-        self.timedeque = deque(maxlen=50)
+        self.deque_time = deque(maxlen=50)
+        self.deque_mean = deque(maxlen=5)
 
     def reset(self):
         self.decoder = Decoder(self.config)
@@ -48,11 +57,12 @@ class Listener:
         return False
         
     def handle_audio(self, buf, frame_count, time_info, status):
+        self.deque_mean.append( audioop.rms(buf,self.sampwidth) )
         if buf:
             t0 = time.time()
             self.decoder.process_raw(buf, False, False)
             # print "process_raw took %.2f ms" % ((time.time()-t0))
-            self.timedeque.append(time.time())
+            self.deque_time.append(time.time())
 
             if self.decoder.hyp() != None:
                 hypstr = str(self.decoder.hyp().hypstr)
@@ -65,27 +75,44 @@ class Listener:
 
         return (buf, pyaudio.paContinue)
 
+    def drawBar(self,val, lower,upper, size=50, extra=""):
+        val = 1.0*val
+        theval = val
+        val = min(val, upper)
+        val = max(val, lower)
+        frac = 1.0*(val-lower)/(upper-lower)
+        fill = int(frac*size)
+        strBuff = "%-5i [" % lower
+        strBuff += "=" * fill
+        strBuff += " " * (size-fill)
+        strBuff += "] %5i [%7.1f]" % (upper, theval)
+        if len(extra) > 0: strBuff += " ... %s" % extra
+        return strBuff
+
     def listen_mic(self):
         p = pyaudio.PyAudio()
 
         self.decoder.start_utt()
-        stream = p.open(format = pyaudio.paInt16, channels=1, rate=self.RATE, input=True, frames_per_buffer=self.CHUNK, stream_callback = self.handle_audio)
+        stream = p.open(format = self.FORMAT, channels=1, rate=self.RATE, input=True, frames_per_buffer=self.CHUNK, stream_callback = self.handle_audio)
         print "starting"
         stream.start_stream()
-        while True:
-            try:
-                pass
-                # print "here"
-                time.sleep(1)
-                perfpct = 100.0*(self.timedeque[-1] - self.timedeque[0]) / (len(self.timedeque)-1) * (1.0*self.RATE / self.CHUNK)
-                print "Running at %.1f%%" % perfpct
-                # print (self.timedeque[-1] - self.timedeque[0]), len(self.timedeque), (1.0*self.RATE / self.CHUNK)
-            except KeyboardInterrupt:
-                stream.stop_stream()
-                stream.close()
-                p.terminate()
-                print "* Killed Process"
-                quit()
+        try:
+            for i in range(10*self.RUN_SECONDS):
+                time.sleep(0.1)
+
+                perfpct = 100.0*(self.deque_time[-1] - self.deque_time[0]) / max(len(self.deque_time)-1,1) * (1.0*self.RATE / self.CHUNK)
+                meanRMS = 1.0*sum(self.deque_mean)/max(len(self.deque_mean),1)
+
+                line = self.drawBar( meanRMS, 0,1250, 50, extra="[%.0f%% speed]" % (perfpct) )
+                sys.stdout.write("\r" + line + " ")
+                sys.stdout.flush()
+
+        except KeyboardInterrupt:
+            print "Terminated"
+
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
         
 
     def listen_file(self, fname, shutup=False):
@@ -133,13 +160,13 @@ if __name__ == '__main__':
     # lst = Listener(hmm_type=0)
     lst = Listener()
 
-    results_sig = lst.listen_file('office_bg_mac_16000_360.wav', shutup=True); print results_sig
-    lst.reset()
-    results_sig = lst.listen_file('home_navsa_pi_16000_120.wav', shutup=True); print results_sig
-    lst.reset()
-    results_sig = lst.listen_file('psr_bg_laptop_16000_240.wav', shutup=True); print results_sig
+    # results_sig = lst.listen_file('office_bg_mac_16000_360.wav', shutup=True); print results_sig
+    # lst.reset()
+    # results_sig = lst.listen_file('home_navsa_pi_16000_120.wav', shutup=True); print results_sig
+    # lst.reset()
+    # results_sig = lst.listen_file('psr_bg_laptop_16000_240.wav', shutup=True); print results_sig
 
-    # lst.listen_mic()
+    lst.listen_mic()
 
     # import itertools, random
     # p = list(itertools.product(
